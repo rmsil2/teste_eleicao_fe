2b07be9f8c4392c1f9f442a13667030cd91e36f7
Marcio Ferreira <silvaferreira.marciojose@gmail.com>
2024-08-28 12:28:59 -0300

    pagina de login não funcional ainda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-/*import { LoginPage } from './login/login.page';
-import { LoginPageRoutingModule } from './login/login-routing.module';*/
+/*import { LoginPageRoutingModule } from './login/login-routing.module';
+import { LoginPageModule } from './login/login.module';*/
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
