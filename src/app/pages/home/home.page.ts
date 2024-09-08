7f61e43564c99e7b8604233eebfb91363ab133d0
Marcio Ferreira <silvaferreira.marciojose@gmail.com>
2024-09-08 11:04:45 -0300

    indicando nome do usuário na página

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PlatformLocation } from '@angular/common';
+import { StorageService } from '../../../services/storage.service';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,18 @@ import { PlatformLocation } from '@angular/common';
 })
 export class HomePage implements OnInit {
 
-    constructor(private pLocation: PlatformLocation) {
+    nome: string;
+
+    constructor(private pLocation: PlatformLocation, public storage: StorageService) {
         history.pushState(null, '', location.href);
         this.pLocation.onPopState(() => { history.pushState(null, '', location.href) });
+    }
+
+    ionViewDidEnter() {
+        let localUser = this.storage.getLocalUser();
+        if (localUser && localUser.nome) {
+            this.nome = localUser.nome;
+        }
     }
 
    ngOnInit() {
